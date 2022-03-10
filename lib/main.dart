49d7820e23dee7e3aa462a4b084036c954594f80
reza mohammadzadeh <mpcreza@gmail.com>
2022-03-10 13:39:36 +0330

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:ffmpeg_kit_flutter_audio/ffmpeg_kit.dart';
+import 'package:ffmpeg_kit_flutter_audio/log.dart';
+import 'package:ffmpeg_kit_flutter_audio/session.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeService();
+  runApp(const MyApp());
+}
+
+Future<void> initializeService() async {
+  final service = FlutterBackgroundService();
+  await service.configure(
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStart,
+      autoStart: true,
+      isForegroundMode: false,
+    ),
+    iosConfiguration: IosConfiguration(
+      autoStart: false,
+      onForeground: onStart,
+      onBackground: onIosBackground,
+    ),
+  );
+}
+
+void onIosBackground() {
+  WidgetsFlutterBinding.ensureInitialized();
+  debugPrint('FLUTTER BACKGROUND FETCH');
+}
+
+void onStart() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final service = FlutterBackgroundService();
+  service.onDataReceived.listen((event) {
+    if (event!["action"] == "stopService") {
+      service.stopBackgroundService();
+    }
+
+    if (event["action"] == "run_ffmpeg") {
+      FFmpegKit.executeAsync(
+        '-i file1.mp4 -c:v mpeg4 file2.mp4',
+        (Session session) async {},
+        (Log log) {
+          debugPrint(log.getMessage());
+        },
+      );
+    }
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Run FFmpeg in background'),
+          onPressed: () {
+            FlutterBackgroundService().sendData({'action': 'run_ffmpeg'});
+          },
+        ),
+      ),
+    );
+  }
+}
